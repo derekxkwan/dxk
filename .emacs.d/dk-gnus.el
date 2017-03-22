5686d0a392938d38c9178d4c80687663e9b662bc
Derek Kwan <derek.x.kwan@gmail.com>
2017-03-21 23:38:33 -0700

    Update dk-gnus.el

--- .emacs.d/dk-gnus.el
+++ .emacs.d/dk-gnus.el
@@ -14,8 +14,15 @@
 (setq user-mail-address "user@gmail.com")
 (setq send-mail-function 'smtpmail-send-it)
 
+;threading stuff
 (setq gnus-summary-thread-gathering-function
       'gnus-gather-threads-by-subject)
+(setq gnus-summary-same-subject "")
+(setq gnus-sum-thread-tree-root "")
+(setq gnus-sum-thread-tree-single-indent "")
+(setq gnus-sum-thread-tree-leaf-with-other "+-> ")
+(setq gnus-sum-thread-tree-vertical "|")
+(setq gnus-sum-thread-tree-single-leaf "`-> ")
 
 ;; Make Gnus NOT ignore [Gmail] mailboxes
 (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
