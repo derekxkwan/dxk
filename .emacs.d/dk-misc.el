8fa58a9e08b9c61af91656f6e25dd72e1121a467
Derek Kwan <derek.x.kwan@gmail.com>
2017-03-23 06:44:50 -0700

    Update dk-misc.el

--- .emacs.d/dk-misc.el
+++ .emacs.d/dk-misc.el
@@ -20,6 +20,15 @@
 (global-set-key (kbd "C-p")
     (lambda () (interactive) (forward-line -5)))
 
+;from emacswiki
+(defun goto-match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis, otherwise insert %.
+vi style of % jumping to matching brace."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+(global-set-key (kbd "C-%") 'goto-match-paren)
 
 ; from lord ophidian of stackoverflow
 (defun my-c-mode-common-hook ()
