2ef8cef626354e5a1b60ec0eeb9b76aa92f6fac8
Derek Kwan <derek.x.kwan@gmail.com>
2017-03-23 22:00:00 -0700

    Update dk-godmode.el

--- .emacs.d/dk-godmode.el
+++ .emacs.d/dk-godmode.el
@@ -1,2 +1,4 @@
 (require 'god-mode)
-(global-set-key (kbd "<escape>") 'god-local-mode)
+;local mode is just too darn confusing to remember
+;(global-set-key (kbd "<escape>") 'god-local-mode)
+(global-set-key (kbd "<escape>") 'god-mode-all)
