12bc299cb83228c63c1ab1f25f81a3c5afac751e
Derek Kwan <derek.x.kwan@gmail.com>
2017-03-24 19:52:01 -0700

    Update dk-godmode.el

--- .emacs.d/dk-godmode.el
+++ .emacs.d/dk-godmode.el
@@ -1,6 +1,8 @@
 (require 'god-mode)
-;local mode is just too darn confusing to remember
-;(global-set-key (kbd "<escape>") 'god-local-mode)
+
+::KEYBINDINGS
+;;local mode is just too darn confusing to remember
+;;(global-set-key (kbd "<escape>") 'god-local-mode)
 (global-set-key (kbd "<escape>") 'god-mode-all)
 (define-key god-local-mode-map (kbd ".") 'repeat)
 (define-key god-local-mode-map (kbd "i") 'god-local-mode)
@@ -8,3 +10,13 @@
 (global-set-key (kbd "C-x C-2") 'split-window-below)
 (global-set-key (kbd "C-x C-3") 'split-window-right)
 (global-set-key (kbd "C-x C-0") 'delete-window)
+
+
+;;CURSOR
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only)
+                        'hollow
+                      'box)))
+
+(add-hook 'god-mode-enabled-hook 'my-update-cursor)
+(add-hook 'god-mode-disabled-hook 'my-update-cursor)
