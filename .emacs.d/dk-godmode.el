6f5de206c90e728cb6a383cbf57182c586a681ae
Derek Kwan <derek.x.kwan@gmail.com>
2017-03-24 19:34:45 -0700

    adding some keybindings

--- .emacs.d/dk-godmode.el
+++ .emacs.d/dk-godmode.el
@@ -2,3 +2,9 @@
 ;local mode is just too darn confusing to remember
 ;(global-set-key (kbd "<escape>") 'god-local-mode)
 (global-set-key (kbd "<escape>") 'god-mode-all)
+(define-key god-local-mode-map (kbd ".") 'repeat)
+(define-key god-local-mode-map (kbd "i") 'god-local-mode)
+(global-set-key (kbd "C-x C-1") 'delete-other-windows)
+(global-set-key (kbd "C-x C-2") 'split-window-below)
+(global-set-key (kbd "C-x C-3") 'split-window-right)
+(global-set-key (kbd "C-x C-0") 'delete-window)
