e714f5de13c6528d927831aae3ce584b6fcaac28
Derek Kwan <derek.x.kwan@gmail.com>
2017-04-26 16:30:46 -0700

    Update dk-misc.el

--- .emacs.d/dk-misc.el
+++ .emacs.d/dk-misc.el
@@ -39,11 +39,39 @@ vi style of % jumping to matching brace."
 (ido-mode 1)
 (setq ido-separator "\n")
 
-;; CHANGING WINDOW SPLIT SIZES
-;; from phils of stackoverflow
+;; WINDOW SPLIT STUFF
+;; changing window split sizes from phils of stackoverflow
 (defun halve-other-window-height ()
   "Expand current window to use half of the other window's lines."
   (interactive)
   (enlarge-window (/ (window-height (next-window)) 2)))
 
 (global-set-key (kbd "C-c v") 'halve-other-window-height)
+
+;;toggle window split mode from jeffdwork of emacswiki
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+	     (next-win-buffer (window-buffer (next-window)))
+	     (this-win-edges (window-edges (selected-window)))
+	     (next-win-edges (window-edges (next-window)))
+	     (this-win-2nd (not (and (<= (car this-win-edges)
+					 (car next-win-edges))
+				     (<= (cadr this-win-edges)
+					 (cadr next-win-edges)))))
+	     (splitter
+	      (if (= (car this-win-edges)
+		     (car (window-edges (next-window))))
+		  'split-window-horizontally
+		'split-window-vertically)))
+	(delete-other-windows)
+	(let ((first-win (selected-window)))
+	  (funcall splitter)
+	  (if this-win-2nd (other-window 1))
+	  (set-window-buffer (selected-window) this-win-buffer)
+	  (set-window-buffer (next-window) next-win-buffer)
+	  (select-window first-win)
+	  (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "C-x |") 'toggle-window-split)
