22fed6a4c0f1d52859005d785ccb31d9d5ee5deb
Derek Kwan <derek.x.kwan@gmail.com>
2017-10-01 20:07:44 -0700

    fix order

--- .emacs.d/dk-org.el
+++ .emacs.d/dk-org.el
@@ -6,10 +6,13 @@
 ;; Enable transient mark mode
 (transient-mark-mode 1)
 
-(define-key org-mode-map "\M-q" 'toggle-truncate-lines)
 ;;;;org-mode configuration
 ;; Enable org-mode
 (require 'org)
 ;; Make org-mode work with files ending in .org
 ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 ;; The above is the default in recent emacsen
+
+(local-set-key "\M-\C-g" 'org-plot/gnuplot)
+
+(define-key org-mode-map "\M-q" 'toggle-truncate-lines)
