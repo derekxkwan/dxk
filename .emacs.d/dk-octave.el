10f65b93cb0afe7f4f50b0b7dcf3de1866a40c7c
Derek Kwan <derek.x.kwan@gmail.com>
2018-01-01 03:19:03 -0800

    Create dk-octave.el

---
+++ .emacs.d/dk-octave.el
@@ -0,0 +1,14 @@
+;; all from gnu website
+;; add .m hook
+(autoload 'octave-mode "octave-mod" nil t)
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+
+;; abbrevs, auto-fill, font-lock, etc.
+
+(add-hook 'octave-mode-hook
+          (lambda ()
+            (abbrev-mode 1)
+            (auto-fill-mode 1)
+            (if (eq window-system 'x)
+                (font-lock-mode 1))))
