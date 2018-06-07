1b9230e3755ebe02b212219943b8da570aa3a9ea
Derek Kwan <derek.x.kwan@gmail.com>
2018-06-07 16:43:38 -0700

    Update dk-ruby.el

--- .emacs.d/dk-ruby.el
+++ .emacs.d/dk-ruby.el
@@ -1,2 +1,6 @@
 (autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
+
+(defun jruby ()
+  (interactive)
+  (inf-ruby "jruby"))
