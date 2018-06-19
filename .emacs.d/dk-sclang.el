b4e59ae7704ce3155a39e8a710c45f192419b4d1
Derek Kwan <derek.x.kwan@gmail.com>
2018-06-19 03:48:09 -0700

    Update dk-sclang.el

--- .emacs.d/dk-sclang.el
+++ .emacs.d/dk-sclang.el
@@ -1,1 +1,10 @@
+(require 'sclang)
+(require 'w3m)
 (setq auto-scroll-post-buffer t)
+
+(eval-after-load "w3m"
+ '(progn
+ (define-key w3m-mode-map [left] 'backward-char)
+ (define-key w3m-mode-map [right] 'forward-char)
+ (define-key w3m-mode-map [up] 'previous-line)
+ (define-key w3m-mode-map [down] 'next-line)))
