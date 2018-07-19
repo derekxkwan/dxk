91e59af703af776e08a34e62ce04e9018e4d8c6b
Derek Kwan <derek.x.kwan@gmail.com>
2018-07-19 04:16:32 -0700

    Update dk-slime.el

--- .emacs.d/dk-slime.el
+++ .emacs.d/dk-slime.el
@@ -1,4 +1,4 @@
 (setq inferior-lisp-program "ros -Q run")
 ;;(load (expand-file-name "~/.roswell/helper.el"))
 (require 'slime)
-(slime-setup '(slime-fancy slime-tramp slime-asdf))
+(slime-setup '(slime-tramp slime-asdf))
