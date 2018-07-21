21ecd4c2c522c9822346a5d718aa5d185e8f4133
Derek Kwan <derek.x.kwan@gmail.com>
2018-07-21 06:13:08 -0700

    Update dk-geiser.el

--- .emacs.d/dk-geiser.el
+++ .emacs.d/dk-geiser.el
@@ -1,2 +1,4 @@
-(setq geiser-repl-read-only-prompt-p nil)
-(setq geiser-racket--prompt-regexp "<pkgs>.*> \\|\\(mzscheme\\|racket\\)@[^ ]*> ")
+;;(setq geiser-repl-read-only-prompt-p nil)
+;;(setq geiser-racket--prompt-regexp "<pkgs>.*> \\|\\(mzscheme\\|racket\\)@[^ ]*> ")
+(setq geiser-racket--prompt-regexp "(-|racket@|\"[^ ]*\.rkt\")[^ ]*> ")
+(setq geiser-racket-init-file "~/.racketrc")
