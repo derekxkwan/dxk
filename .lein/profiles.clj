fcd9074986438ca655547cf45055331b4f7f5cd3
Derek Kwan <derek.x.kwan@gmail.com>
2018-07-23 15:38:27 -0700

    needs to be in brackets

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
-:user
-{
-  :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]
-}
+{:user
+  {
+    :jvm-opts ["-Dclojure.server.repl={:port 5555 :accept clojure.core.server/repl}"]
+  }
+ }
