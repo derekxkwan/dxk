5f5583040b0285b1d92a5d5673e62cd7dc20a64e
Derek Kwan <derek.x.kwan@gmail.com>
2018-08-04 01:19:05 -0700

    Update dk-company.el

--- .emacs.d/dk-company.el
+++ .emacs.d/dk-company.el
@@ -1,1 +1,16 @@
 (add-hook 'after-init-hook 'global-company-mode)
+
+;;borrowed from emacs-wiki
+(defun company-complete-or-indent ()
+  (interactive)
+  (if (company-manual-begin)
+      (company-complete-common)
+    (indent-according-to-mode)))
+
+(defun company-indent-or-complete ()
+  (interactive)
+  (if (looking-at "\\_>")
+      (company-complete-common)
+    (indent-according-to-mode)))
+
+(global-set-key "\t" 'company-indent-or-complete)
