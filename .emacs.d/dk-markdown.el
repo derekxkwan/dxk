a7a0a4ed95f8ac3625c3cc243124919627d9c919
Derek Kwan <derek.x.kwan@gmail.com>
2020-02-29 17:09:57 -0800

    Create dk-markdown.el

---
+++ .emacs.d/dk-markdown.el
@@ -0,0 +1,8 @@
+(setq markdown-xhtml-header-content
+      "<style type='text/css'>
+html,body {font-family: 'Geneva', 'Verdana', sans-serif;
+background-color:white' }
+img {border:none;}
+a { text-decoration: none; }
+a:hover { text-decoration: underline; }
+</style>")
